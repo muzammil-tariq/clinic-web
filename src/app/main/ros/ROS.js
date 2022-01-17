d442ac2f21e6c0ab6f494851c7b9dd1ea8a9951e
hassanfarooq644 <36379912+hassanfarooq644@users.noreply.github.com>
2022-01-18 00:59:39 +0500

    ros code complete

--- src/app/main/ros/ROS.js
+++ src/app/main/ros/ROS.js
@@ -472,6 +472,30 @@ const ROS = () => {
 		hematesis: false,
 		bloodShoot: false,
 		allergicReaction: false,
+		seizures: false,
+		burningSensations: false,
+		sensationLoss: false,
+		tinglingSensation: false,
+		recurrentInfection: false,
+		nervousnessAnxiety: false,
+		depression: false,
+		frequency: false,
+		urgency: false,
+		incontenence: false,
+		hematuria: false,
+		heatIntolerence: false,
+		coldIntolerence: false,
+		polydypsia: false,
+		polyuria: false,
+		polyphagia: false,
+		weightGain: false,
+		stiffness: false,
+		jointPain: false,
+		musclePain: false,
+		abnormalBleeding: false,
+		brusing: false,
+		menorrhagia: false,
+		lumpBreast: false,
 		patientId: ''
 	});
 
@@ -631,6 +655,417 @@ const ROS = () => {
 											label="Growths"
 											name="growths"
 											value={rosData.growths}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>GastroIntestinal</b>
+											</Typography>
+										</div>
+										<Item
+											label="Abdominal Pain"
+											name="abdominalPain"
+											value={rosData.abdominalPain}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Heart burn/Gerd"
+											name="heartBurn"
+											value={rosData.heartBurn}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Nausea/Vomiting"
+											name="nausea"
+											value={rosData.nausea}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Constipation"
+											name="constipation"
+											value={rosData.constipation}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Diarrhea"
+											name="diarrhea"
+											value={rosData.diarrhea}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Hematesis"
+											name="hematesis"
+											value={rosData.hematesis}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Blood in Stool"
+											name="bloodShoot"
+											value={rosData.bloodShoot}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Eye</b>
+											</Typography>
+										</div>
+										<Item
+											label="Visual Changes"
+											name="visualChanges"
+											value={rosData.visualChanges}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Red eye"
+											name="redEye"
+											value={rosData.redEye}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Neurological</b>
+											</Typography>
+										</div>
+										<Item
+											label="Numbness/Tingling Sensation"
+											name="tinglingSensation"
+											value={rosData.tinglingSensation}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Sensation Loss"
+											name="sensationLoss"
+											value={rosData.sensationLoss}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Burning Sensations"
+											name="burningSensations"
+											value={rosData.burningSensations}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Seizures"
+											name="seizures"
+											value={rosData.seizures}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>ENT</b>
+											</Typography>
+										</div>
+										<Item
+											label="Hearing Loss"
+											name="hearingLoss"
+											value={rosData.hearingLoss}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Nasal discharge"
+											name="nasalDischarge"
+											value={rosData.nasalDischarge}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Epitaxis"
+											name="epitaxis"
+											value={rosData.epitaxis}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Dysphagia"
+											name="dysphagia"
+											value={rosData.dysphagia}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Allergies/ Immunity</b>
+											</Typography>
+										</div>
+										<Item
+											label="Allergic Reaction"
+											name="allergicReaction"
+											value={rosData.allergicReaction}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Recurrent Infection"
+											name="recurrentInfection"
+											value={rosData.recurrentInfection}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Psychatric</b>
+											</Typography>
+										</div>
+										<Item
+											label="Nervousness/Anxiety"
+											name="nervousnessAnxiety"
+											value={rosData.nervousnessAnxiety}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Depression"
+											name="depression"
+											value={rosData.depression}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>CVS</b>
+											</Typography>
+										</div>
+										<Item
+											label="Chest Pain/Discomfort"
+											name="chestPain"
+											value={rosData.chestPain}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Palpitaion / Arrythmia"
+											name="palpitations"
+											value={rosData.palpitations}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Shortness of Breath"
+											name="shortnessOfBreath"
+											value={rosData.shortnessOfBreath}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Perepheral Edema"
+											name="perepheralEdema"
+											value={rosData.perepheralEdema}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Orthopnea"
+											name="orthopnea"
+											value={rosData.orthopnea}
+											onChange={handleChange}
+										/>
+										<Item label="PND" name="pnd" value={rosData.pnd} onChange={handleChange} />
+										<Item
+											label="Syncope"
+											name="syncope"
+											value={rosData.syncope}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Vericose Vaiens"
+											name="vericoseVaiens"
+											value={rosData.vericoseVaiens}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Cramping in Legs"
+											name="crampingInLegs"
+											value={rosData.crampingInLegs}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Others"
+											name="other"
+											value={rosData.other}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Genitourinary</b>
+											</Typography>
+										</div>
+										<Item
+											label="Frequency"
+											name="frequency"
+											value={rosData.frequency}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Urgency"
+											name="urgency"
+											value={rosData.urgency}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Incontenence"
+											name="incontenence"
+											value={rosData.incontenence}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Dysuria"
+											name="dysuria"
+											value={rosData.dysuria}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Hematuria"
+											name="hematuria"
+											value={rosData.hematuria}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Endocrine</b>
+											</Typography>
+										</div>
+										<Item
+											label="Heat intolerence"
+											name="heatIntolerence"
+											value={rosData.heatIntolerence}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Cold Intolerence"
+											name="coldIntolerence"
+											value={rosData.coldIntolerence}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Polydypsia"
+											name="polydypsia"
+											value={rosData.polydypsia}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Polyuria"
+											name="polyuria"
+											value={rosData.polyuria}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Polyphagia"
+											name="polyphagia"
+											value={rosData.polyphagia}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Weight Gain"
+											name="weightGain"
+											value={rosData.weightGain}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Musculoskeletal</b>
+											</Typography>
+										</div>
+										<Item
+											label="Joint Pain/Swelling"
+											name="heatIntolerence"
+											value={rosData.heatIntolerence}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Stiffness"
+											name="stiffness"
+											value={rosData.stiffness}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Muscle pain"
+											name="musclePain"
+											value={rosData.musclePain}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<div class="grid grid-cols-2 gap-4">
+								<div class="p-6">
+									<div class="w-full border p-20 rounded border-gray-400 bg-white">
+										<div class="py-5">
+											<Typography variant="subtitle2">
+												<b>Haematology / Lympahtics</b>
+											</Typography>
+										</div>
+										<Item
+											label="Menorrhagia"
+											name="menorrhagia"
+											value={rosData.menorrhagia}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Vaginal Discharge"
+											name="vaginalDischarge"
+											value={rosData.vaginalDischarge}
+											onChange={handleChange}
+										/>
+										<Item
+											label="Lump Breast"
+											name="lumpBreast"
+											value={rosData.lumpBreast}
 											onChange={handleChange}
 										/>
 									</div>
